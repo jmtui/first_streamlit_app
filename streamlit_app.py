51275ff1c5d1d13bf992e21c1df1deb8c47d0de5
jmtui <35045143+jmtui@users.noreply.github.com>
2022-07-20 13:47:53 +0100

    Update streamlit_app.py
    Changed header slightly

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit
 
-streamlit.title('My Parents New Heathy Diner')
+streamlit.title('My Mom''s New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast favourites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+
